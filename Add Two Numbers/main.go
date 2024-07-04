eb23e69a28c8e1c599e7a78e7f92b78fe43420cf
gonyango <grantononyango@gmail.com>
2024-07-04 12:39:30 +0300

    Feat: structure of a listNode

--- Add Two Numbers/main.go
+++ Add Two Numbers/main.go
@@ -2,38 +2,19 @@ package main
 
 import "fmt"
 
-type Node struct{
-	data int
-	nextNode *Node
+type ListNode struct{
+	Val int
+	Next *ListNode
 }
 
-type LinkedList struct{
-	headNode *Node
 }
 
 
-func (l *LinkedList) addNode(num int){
-	newNode := &Node{data:num}
-	currentNode := l.headNode
-	l.headNode = newNode
-	l.headNode.nextNode = currentNode
 }
 
-func (l *LinkedList) printNode() []int{
-	currentNode := l.headNode
-	for currentNode != nil{
-		fmt.Println(currentNode.data)
-		currentNode = currentNode.nextNode
 	}
-	return []int{}
 }
 
-func main2() {
-	linkList := &LinkedList{}
 
-	linkList.addNode(47)
-	linkList.addNode(43)
-	linkList.addNode(50)
 
-	linkList.printNode()
 }
